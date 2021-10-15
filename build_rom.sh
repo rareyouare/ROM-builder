0b12e4f27e2d8ade27122ac45020cf85531a1ebe
rareyouare <92556411+rareyouare@users.noreply.github.com>
2021-10-15 15:39:24 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/ItoRenz/local_manifest.git --depth 1 -b lineage-18.1-oss .repo/local_manifests
+git clone https://github.com/rareyouare/local_manifest.git --depth 1 -b default .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch lineage_rova-userdebug
+lunch lineage_ulysse-user
 export TZ=Asia/Jakarta
-export KBUILD_BUILD_USER=ItoRenz
-export KBUILD_BUILD_HOST=Tempest
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+curl --upload-file ./out/target/product/ulysse/*.zip https://transfer.sh/*.zip
